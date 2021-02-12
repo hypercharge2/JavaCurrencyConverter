fdeb728e3cb38dbecace22cb4765130dd76172c9
Hypercharge2 <habarney.cny@gmail.com>
2021-02-12 18:51:08 -0500

    Added the dropdowns and submit button

--- Currency Converter/src/convert/Main.java
+++ Currency Converter/src/convert/Main.java
@@ -2,6 +2,8 @@
 
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,22 +20,38 @@ public class Main {
 	
 	JPanel mainpnl = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 	
+	String[] currencies = new String[] { "United States Dollar", "Euro", "Japnese Yen", "Pound Sterling", "Australian Dollar", "Canadian Dolllar", "Swiss Franc", "Chinese Renminbi", "Hong Kong Dollar", "New Zeland Dollar"};     
+	
 	JLabel inpCurL = new JLabel();
-	inpCurL.setText("Input currency:(no dropdown yet)");
+	inpCurL.setText("Input currency:");
+	JComboBox<Object> inputc = new JComboBox<Object>(currencies);
+	
 	
 	JLabel inpamountL = new JLabel("Input Currency Amount:");
 	JFormattedTextField inpamount = new JFormattedTextField();
 	inpamount.setColumns(5);
 	
-	JLabel outputcurrency = new JLabel("Output currency: (No dropdown yet)");
+	JLabel outputcurrencyL = new JLabel("Currency to convert to: (No dropdown yet)");
+	JComboBox<Object> outputc = new JComboBox<Object>(currencies);
+	
+	JButton submit = new JButton();
 	JLabel output = new JLabel("Output: (No output function yet)");
+	
+	
+	
+	
+	
+	
+	
+	
 	mainframe.add(mainpnl);
 	mainpnl.add(inpCurL);
-	//placeholder for when i make the currency dropdown
+	mainpnl.add(inputc);
 	mainpnl.add(inpamountL);
 	mainpnl.add(inpamount);
-	mainpnl.add(outputcurrency);
-	//placeholder for when i make the output currency dropdown
+	mainpnl.add(outputcurrencyL);
+	mainpnl.add(outputc);
+	mainpnl.add(submit);
 	mainpnl.add(output);
 	mainframe.pack();
 	
